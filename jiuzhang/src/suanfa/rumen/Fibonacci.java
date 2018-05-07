66c08925d90d0cf6e44075be0eaa4aa4bc5c5f8d
fei.li@healthbi.com <fei.li@healthbi.com>
2018-05-07 11:46:19 -0700

    Added for Fibonacci with two implementations. 1. Recursion 2. loop

---
+++ jiuzhang/src/suanfa/rumen/Fibonacci.java
@@ -0,0 +1,34 @@
+package suanfa.rumen;
+
+public class Fibonacci {
+    public static int fibonacci(int n) {
+        
+    	if(n == 1) {
+    		return 0;
+    	}
+    	
+    	if(n == 2) {
+    		return 1;
+    	}
+    	
+    	return fibonacci(n-1) + fibonacci(n-2);
+    }
+    
+    public static int fibonacci1(int n) {
+    	int a = 0;
+    	int b = 1;
+    	for(int i = 0; i < n - 1; i ++) {
+    		int c = a + b;
+    		System.out.println("a : "+a +" b : "+b + " total : "+c);
+    		a = b;
+    		b = c;
+    	}
+    	
+    	return a;
+    }
+    
+    public static void main(String[] args) {
+    	System.out.println(fibonacci1(10));
+//    	System.out.println(fibonacci(10));
+    }
+}
